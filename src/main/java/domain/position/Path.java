61df27d4fbe1b052201bbbd0455fee5504402d02
MoonJeWoong <munjin0201@naver.com>
2023-04-09 17:21:26 +0900

    chore: path 계산 로직 관련 주석 추가

--- src/main/java/domain/position/Path.java
+++ src/main/java/domain/position/Path.java
@@ -19,6 +19,14 @@ public class Path {
     }
 
     private void calculatePath(Position start, Position end) {
+        /*
+        path를 계산하는 로직이 복잡해서 직관적으로 이해가 안되실 것 같아 주석을 덧붙입니다.
+        start end 두 포지션을 받아서 각 row, column 간 차이를 구합니다.
+        그 다음 row와 column 중 더 큰 차이가 있는 곳의 좌표 값을 1씩 증감시킨 위치를 path에 추가합니다.
+        만약 정대각선의 경우 row, column 값을 동시에 1씩 증감하면서 위치를 path에 추가합니다.
+        모든 케이스 별로 메서드를 나누면 복잡한 수식은 리팩토링할 수 있겠지만 시간 상 문제로 생략했습니다. 감안 부탁드립니다. :)
+         */
+
         int rowGap = start.calculateRowGap(end);
         int columnGap = start.calculateColumnGap(end);
 
